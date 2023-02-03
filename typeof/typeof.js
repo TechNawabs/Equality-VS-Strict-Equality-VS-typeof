58ca2b085aafb681ee1df6841c39aacabce252d3
Aditya Kumar <adityaverma.dev@gmail.com>
2023-02-03 19:38:34 +0530

    details adde

---
+++ typeof/typeof.js
@@ -0,0 +1,12 @@
+var a;
+var b = null;
+
+console.log(typeof a);
+console.log(typeof a === undefined);
+console.log(typeof a === "undefined");
+
+console.log(typeof b);
+
+console.log(typeof{});
+
+console.log(typeof[]);
